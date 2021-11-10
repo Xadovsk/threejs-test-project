f19188d25b5f12770fad684c7fb501179a465177
Xadovsk <56688906+Xadovsk@users.noreply.github.com>
2021-11-10 10:59:14 -0300

    added ar js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {EngineComponent} from './engine/engine.component';
@@ -24,7 +24,8 @@ import {UiComponent} from './ui/ui.component';
   providers: [],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule {
 }
